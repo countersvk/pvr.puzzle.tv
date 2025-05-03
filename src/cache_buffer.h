27e509e86f0286d23d718ea5181f660ab1f68e28
countersvk <167197390+countersvk@users.noreply.github.com>
2025-05-03 12:42:28 +0200

    Update cache_buffer.h

--- src/cache_buffer.h
+++ src/cache_buffer.h
@@ -1,87 +1,63 @@
-/*
- *
- *   Copyright (C) 2017 Sergey Shramchenko
- *   https://github.com/srg70/pvr.puzzle.tv
- *
- *  This Program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
- *
- *  This Program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- *  http://www.gnu.org/copyleft/gpl.html
- *
- */
+#pragma once
 
-#ifndef IChunckedBuffer_h
-#define IChunckedBuffer_h
+#include <cstdint>
+#include <stdexcept>
+#include <memory>
+#include <chrono>
+#include <system_error>
 
+namespace Buffers {
 
-#include "p8-platform/os.h"
-#include <stdint.h>
+    enum class SeekOrigin {
+        Begin,
+        Current,
+        End
+    };
 
-namespace Buffers
-{
-    
-    // Interface for synchronized data stream
-    // New data always apeended to the end of stream
-    // Real data capacity may be limited.
-    // In case of limited capacity stream will provide partial amount of data inside chache window (latest, i.e. from the tail),
-    // although the total length will always correspond to amount of written data.
-    
     class ICacheBuffer {
     public:
-        ICacheBuffer() {};
+        using Clock = std::chrono::system_clock;
+        using TimePoint = Clock::time_point;
+        using Duration = Clock::duration;
 
-        // Initialize empty buffer (aka constructor)
-        virtual  void Init() = 0;
-        virtual  uint32_t UnitSize() = 0;
-        
-        // Read interface
-        // Seak read position within cache window
-        virtual int64_t Seek(int64_t iFilePosition, int iWhence) = 0;
-        // Virtual steream lenght.
-        virtual int64_t Length() = 0;
-        // Current read position
-        virtual int64_t Position() = 0;
-        // Reads data from Position(),
-        virtual ssize_t Read(void* lpBuf, size_t uiBufSize) = 0;
-        
-        // Write interface
-        virtual bool LockUnitForWrite(uint8_t** pBuf) = 0;
-        virtual void UnlockAfterWriten(uint8_t* pBuf, ssize_t writtenBytes = -1) = 0;
-        
-        virtual time_t StartTime() const = 0;
-        virtual time_t EndTime() const = 0;
-        virtual float FillingRatio() const = 0;
+        ICacheBuffer() = default;
+        virtual ~ICacheBuffer() = default;
 
-        virtual ~ICacheBuffer() {};
-        
-    private:
-        ICacheBuffer(const ICacheBuffer&) = delete ;                    //disable copy-constructor
-        ICacheBuffer& operator=(const ICacheBuffer&) = delete;  //disable copy-assignment
-        
-        
+        // Чтение данных
+        [[nodiscard]] virtual std::int64_t Seek(std::int64_t offset, SeekOrigin origin) = 0;
+        [[nodiscard]] virtual std::int64_t Length() const noexcept = 0;
+        [[nodiscard]] virtual std::int64_t Position() const noexcept = 0;
+        [[nodiscard]] virtual std::size_t Read(std::byte* buffer, std::size_t size) = 0;
+
+        // Запись данных
+        [[nodiscard]] virtual std::span<std::byte> LockForWrite() = 0;
+        virtual void CommitWrite(std::size_t bytes_written) = 0;
+
+        // Метаданные
+        [[nodiscard]] virtual TimePoint StartTime() const noexcept = 0;
+        [[nodiscard]] virtual TimePoint EndTime() const noexcept = 0;
+        [[nodiscard]] virtual float FillRatio() const noexcept = 0;
+
+        // Удаление копирования
+        ICacheBuffer(const ICacheBuffer&) = delete;
+        ICacheBuffer& operator=(const ICacheBuffer&) = delete;
+
+        // Поддержка перемещения
+        ICacheBuffer(ICacheBuffer&&) noexcept = default;
+        ICacheBuffer& operator=(ICacheBuffer&&) noexcept = default;
     };
-    
-    
-    class CacheBufferException : public std::exception
-    {
+
+    class CacheBufferError : public std::runtime_error {
     public:
-        CacheBufferException(const char* reason = "") : r(reason){}
-        virtual const char* what() const noexcept {return r;}
+        using std::runtime_error::runtime_error;
         
+        explicit CacheBufferError(std::error_code ec)
+            : std::runtime_error(ec.message()), code(ec) {}
+
+        [[nodiscard]] std::error_code errorCode() const noexcept { return code; }
+
     private:
-        const char* r;
+        std::error_code code;
     };
 
-}
-
-#endif /* IChunckedBuffer_h */
+} // namespace Buffers
