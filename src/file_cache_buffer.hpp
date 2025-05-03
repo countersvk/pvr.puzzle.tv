c399c129d6003c01fa8dae484be0c4a4d97b30c0
countersvk <167197390+countersvk@users.noreply.github.com>
2025-05-03 13:00:40 +0200

    Update file_cache_buffer.hpp

--- src/file_cache_buffer.hpp
+++ src/file_cache_buffer.hpp
@@ -1,99 +1,80 @@
-/*
- *
- *   Copyright (C) 2017 Sergey Shramchenko
- *   https://github.com/srg70/pvr.puzzle.tv
- *
- *  This Program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
- *
- *  This Program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- *  http://www.gnu.org/copyleft/gpl.html
- *
- */
-
-
-#ifndef __file_cache_buffer_hpp__
-#define __file_cache_buffer_hpp__
+#ifndef FILE_CACHE_BUFFER_HPP
+#define FILE_CACHE_BUFFER_HPP
 
 #include <memory>
 #include <string>
 #include <vector>
 #include <deque>
+#include <mutex>
+#include <filesystem>
 #include "cache_buffer.h"
-#include "p8-platform/threads/mutex.h"
 
+namespace Buffers {
 
-namespace Buffers
-{
+class CAddonFile;
+
+class FileCacheBuffer : public ICacheBuffer {
+public:
+    static constexpr uint32_t STREAM_READ_BUFFER_SIZE = 32768; // 32KB
+    static constexpr uint32_t CHUNK_FILE_SIZE_LIMIT = STREAM_READ_BUFFER_SIZE * 4096; // 128MB
+
+    // Read-Write constructor
+    FileCacheBuffer(std::filesystem::path bufferCacheDir, uint8_t sizeFactor, bool autoDelete = true);
     
-    class CAddonFile;
+    // Read-Only constructor
+    explicit FileCacheBuffer(std::filesystem::path bufferCacheDir);
     
-    class FileCacheBuffer : public ICacheBuffer
-    {
-    public:
-        static const uint32_t STREAM_READ_BUFFER_SIZE = 1024 * 32; // 32K input read buffer
-        static const  uint32_t CHUNK_FILE_SIZE_LIMIT = (STREAM_READ_BUFFER_SIZE * 1024) * 4; // 128MB chunk
+    virtual ~FileCacheBuffer() override;
 
-        // Read-Write
-        FileCacheBuffer( const std::string &bufferCacheDir, uint8_t  sizeFactor , bool autoDelete = true);
-        // ReadOnly
-        FileCacheBuffer(const std::string& bufferCacheDir);
-        virtual  void Init();
-        virtual  uint32_t UnitSize();
+    // ICacheBuffer interface implementation
+    void Init() override;
+    uint32_t UnitSize() noexcept override;
+    
+    int64_t Seek(int64_t iFilePosition, int iWhence) override;
+    int64_t Length() noexcept override;
+    int64_t Position() noexcept override;
+    ssize_t Read(void* lpBuf, size_t uiBufSize) override;
+    
+    bool LockUnitForWrite(uint8_t** pBuf) override;
+    void UnlockAfterWriten(uint8_t* pBuf, ssize_t writtenBytes = -1) override;
 
-        
-        // Read interface
-        // Seak read position within cache window
-        virtual int64_t Seek(int64_t iFilePosition, int iWhence) ;
-        // Virtual steream lenght.
-        virtual int64_t Length();
-        // Current read position
-        virtual int64_t Position();
-        // Reads data from Position(),
-        virtual ssize_t Read(void* lpBuf, size_t uiBufSize);
-        
-        // Write interface
-        virtual bool LockUnitForWrite(uint8_t** pBuf);
-        virtual void UnlockAfterWriten(uint8_t* pBuf, ssize_t writtenBytes = -1);
+    time_t StartTime() const noexcept override { return m_startTime; }
+    time_t EndTime() const noexcept override { return m_endTime; }
+    float FillingRatio() const noexcept override { 
+        return static_cast<float>(m_position - m_begin) / (m_length - m_begin); 
+    }
 
-        virtual time_t StartTime() const {return m_startTime;}
-        virtual time_t EndTime() const {return m_endTime;}
-        virtual float FillingRatio() const {return (float)(m_position - m_begin) / (m_length - m_begin); }
+    // Delete copy operations
+    FileCacheBuffer(const FileCacheBuffer&) = delete;
+    FileCacheBuffer& operator=(const FileCacheBuffer&) = delete;
 
-        ~FileCacheBuffer();
-        
-    private:
-        typedef CAddonFile* ChunkFilePtr;
-        typedef std::deque<ChunkFilePtr > FileChunks;
-        typedef std::deque<std::unique_ptr<CAddonFile> > ChunkFileSwarm;
+private:
+    using ChunkFilePtr = std::unique_ptr<CAddonFile>;
+    using FileChunks = std::deque<ChunkFilePtr>;
+    using ChunkFileSwarm = std::deque<ChunkFilePtr>;
 
-        ChunkFilePtr CreateChunk();
-        unsigned int GetChunkIndexFor(int64_t position);
-        int64_t GetPositionInChunkFor(int64_t position);
-        ssize_t Write(const void* buf, size_t bufferSize);
-        
-        mutable FileChunks m_ReadChunks;
-        ChunkFileSwarm m_ChunkFileSwarm;
-        mutable P8PLATFORM::CMutex m_SyncAccess;
-        int64_t m_length;
-        int64_t m_position;
-        int64_t m_begin;// virtual start of cache
-        const int64_t m_maxSize;
-        std::string m_bufferDir;
-        const bool m_autoDelete;
-        const bool m_isReadOnly;
-        std::unique_ptr<uint8_t> m_chunkForLock;
-        time_t m_startTime;
-        time_t m_endTime;
-    };
-}
-#endif // __file_cache_buffer_hpp__
+    ChunkFilePtr CreateChunk();
+    std::pair<size_t, int64_t> LocateChunk(int64_t position) const;
+    
+    // Member variables
+    mutable std::mutex m_syncAccess;
+    FileChunks m_readChunks;
+    ChunkFileSwarm m_chunkFileSwarm;
+    
+    std::filesystem::path m_bufferDir;
+    const int64_t m_maxSize;
+    const bool m_autoDelete;
+    const bool m_isReadOnly;
+    
+    int64_t m_length = 0;
+    int64_t m_position = 0;
+    int64_t m_begin = 0; // Virtual start of cache
+    
+    std::vector<uint8_t> m_chunkBuffer;
+    time_t m_startTime = 0;
+    time_t m_endTime = 0;
+};
+
+} // namespace Buffers
+
+#endif // FILE_CACHE_BUFFER_HPP
