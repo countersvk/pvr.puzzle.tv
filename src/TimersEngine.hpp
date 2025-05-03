293f464953a24111a7a331697f9cc206dc4b1d3b
countersvk <167197390+countersvk@users.noreply.github.com>
2025-05-03 12:14:15 +0200

    Update TimersEngine.hpp

--- src/TimersEngine.hpp
+++ src/TimersEngine.hpp
@@ -1,62 +1,56 @@
-/*
-*
-*   Copyright (C) 2018 Sergey Shramchenko
-*   https://github.com/srg70/pvr.puzzle.tv
-*
-*  This Program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2, or (at your option)
-*  any later version.
-*
-*  This Program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with XBMC; see the file COPYING.  If not, write to
-*  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-*  http://www.gnu.org/copyleft/gpl.html
-*
-*/
+#ifndef TIMERS_ENGINE_H
+#define TIMERS_ENGINE_H
 
-#ifndef __TimersEngine_hpp__
-#define __TimersEngine_hpp__
-
-#include <memory>
+#include <atomic>
 #include <set>
-#include "addon.h"
-#include "p8-platform/threads/threads.h"
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <kodi/addon/pvr/Timers.h>
+#include "ITimersEngine.h"
 
 namespace Engines {
-    
-    class Timer;
-    
-    class TimersEngine : public ITimersEngine, public P8PLATFORM::CThread
-    {
-    public:
-        TimersEngine(ITimersEngineDelegate* delegate);
-        int GetTimersAmount(void) override;
-        PVR_ERROR AddTimer(const kodi::addon::PVRTimer& timer) override;
-        PVR_ERROR GetTimers(kodi::addon::PVRTimersResultSet& results) override;
-        PVR_ERROR DeleteTimer(const kodi::addon::PVRTimer& timer, bool forceDelete) override;
-        PVR_ERROR UpdateTimer(const kodi::addon::PVRTimer& timer) override;
-        virtual ~TimersEngine();
-    private:
-        typedef std::unique_ptr<Timer> TimerPtr;
-        static bool CompareTimerPtr(const TimerPtr &left, const TimerPtr &right);
-        typedef std::multiset<TimerPtr, bool (*)(const TimerPtr &left, const TimerPtr &right)> Timers;
-        
-        void LoadCache();
-        void SaveCache() const;
-        
-        void *Process() override;
-        
-        P8PLATFORM::CEvent m_checkTimers;
-        Timers  m_timers;
-        ITimersEngineDelegate* m_delegate;
-    };
-    
-}
 
-#endif /* TimersEngine_hpp */
+class Timer;
+class ITimersEngineDelegate;
+
+class TimersEngine : public ITimersEngine
+{
+public:
+    explicit TimersEngine(ITimersEngineDelegate* delegate);
+    ~TimersEngine() override;
+
+    // Запрет копирования и присваивания
+    TimersEngine(const TimersEngine&) = delete;
+    TimersEngine& operator=(const TimersEngine&) = delete;
+
+    int GetTimersAmount() override;
+    PVR_ERROR AddTimer(const kodi::addon::PVRTimer& timer) override;
+    PVR_ERROR GetTimers(kodi::addon::PVRTimersResultSet& results) override;
+    PVR_ERROR DeleteTimer(const kodi::addon::PVRTimer& timer, bool forceDelete) override;
+    PVR_ERROR UpdateTimer(const kodi::addon::PVRTimer& timer) override;
+
+private:
+    struct TimerCompare {
+        bool operator()(const std::unique_ptr<Timer>& a, 
+                       const std::unique_ptr<Timer>& b) const;
+    };
+
+    using TimersSet = std::set<std::unique_ptr<Timer>, TimerCompare>;
+
+    void WorkerMain(std::stop_token stopToken);
+    void LoadCache();
+    void SaveCache() const;
+
+    mutable std::mutex m_mutex;
+    std::condition_variable_any m_cv;
+    TimersSet m_timers;
+    ITimersEngineDelegate* m_delegate;
+    std::jthread m_worker;
+    std::atomic_bool m_cacheModified{false};
+};
+
+} // namespace Engines
+
+#endif // TIMERS_ENGINE_H
