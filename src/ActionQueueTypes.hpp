858bb82350d4d4a53dad07d7986ed931e0cb0995
countersvk <167197390+countersvk@users.noreply.github.com>
2025-05-03 09:40:55 +0200

    Update ActionQueueTypes.hpp

--- src/ActionQueueTypes.hpp
+++ src/ActionQueueTypes.hpp
@@ -1,45 +1,41 @@
-//
-//  ActionQueueTypes.hpp
-//  pvr.puzzle.tv
-//
-//  Created by Sergey Shramchenko on 24/10/2018.
-//  Copyright © 2018 Home. All rights reserved.
-//
-
-#ifndef ActionQueueTypes_h
-#define ActionQueueTypes_h
+#ifndef ACTION_QUEUE_TYPES_H
+#define ACTION_QUEUE_TYPES_H
 
 #include <functional>
+#include <exception>
+#include <memory>
 
 namespace ActionQueue
 {
-    
+    enum class ActionStatus 
+    {
+        Completed,
+        Cancelled,
+        Failed
+    };
+
+    struct ActionResult
+    {
+        ActionStatus status;
+        std::exception_ptr exception;
+        
+        explicit ActionResult(ActionStatus s, 
+                             std::exception_ptr e = nullptr) noexcept
+            : status(s), exception(std::move(e)) 
+        {}
+    };
+
     class IActionQueueItem
     {
     public:
         virtual void Perform() = 0;
         virtual void Cancel() = 0;
-        virtual ~IActionQueueItem() {}
-    protected:
+        virtual ~IActionQueueItem() = default;
     };
-    
-    typedef enum {
-        kActionCompleted,
-        kActionCancelled,
-        kActionFailed
-    } ActionCompletionStatus;
-    
-    struct ActionResult
-    {
-        ActionCompletionStatus status;
-        std::exception_ptr exception;
-        
-        ActionResult(ActionCompletionStatus s, std::exception_ptr e = nullptr)
-        : status(s), exception(e)
-        {}
-    };
-    typedef std::function<void(void)> TAction;
-    typedef std::function<void(const ActionResult&)> TCompletion;
+
+    // Modern C++ type aliases
+    using TAction = std::function<void()>;
+    using TCompletion = std::function<void(const ActionResult&)>;
 }
 
-#endif /* ActionQueueTypes_h */
+#endif // ACTION_QUEUE_TYPES_H
