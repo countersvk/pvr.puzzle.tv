8c3e80ae31b60154be3e93c64ce5a1f8389c4872
countersvk <167197390+countersvk@users.noreply.github.com>
2025-05-03 13:19:13 +0200

    Update globals.hpp

--- src/globals.hpp
+++ src/globals.hpp
@@ -1,47 +1,68 @@
-/*
- *
- *   Copyright (C) 2017 Sergey Shramchenko
- *   https://github.com/srg70/pvr.puzzle.tv
- *
- *  This Program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
- *
- *  This Program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- *  http://www.gnu.org/copyleft/gpl.html
- *
- */
+#ifndef GLOBALS_HPP
+#define GLOBALS_HPP
 
-#ifndef __globals_hpp__
-#define __globals_hpp__
-
-//#include <string>
-//#include "kodi/libXBMC_pvr.h"
-//#include "kodi/libXBMC_addon.h"
-//#include "kodi/libKODI_guilib.h"
-#include "addon.h"
-#include "kodi/Filesystem.h"
+#include <memory>
+#include <string_view>
+#include <mutex>
+#include <kodi/AddonBase.h>
+#include <kodi/Filesystem.h>
 
 namespace Globals {
 
-    extern IAddonDelegate* const& PVR;
-//    extern ADDON::CHelper_libXBMC_addon* const& XBMC;
-//    extern CHelper_libKODI_guilib* const& GUI;
-    
-    void LogError(const char *format, ... );
-    void LogInfo(const char *format, ... );
-    void LogNotice(const char *format, ... );
-    void LogDebug(const char *format, ... );
-    
-    kodi::vfs::CFile* XBMC_OpenFile(const std::string& path, unsigned int flags = 0);
+class AddonManager {
+public:
+    static AddonManager& instance() {
+        static AddonManager instance;
+        return instance;
+    }
 
+    void init(kodi::addon::CInstancePVRClient* pvr) {
+        std::lock_guard lock(mutex_);
+        pvr_instance_ = pvr;
+    }
+
+    void cleanup() {
+        std::lock_guard lock(mutex_);
+        pvr_instance_ = nullptr;
+    }
+
+    kodi::addon::CInstancePVRClient* pvr() const {
+        return pvr_instance_;
+    }
+
+    AddonManager(const AddonManager&) = delete;
+    AddonManager& operator=(const AddonManager&) = delete;
+
+private:
+    AddonManager() = default;
+    ~AddonManager() = default;
+
+    kodi::addon::CInstancePVRClient* pvr_instance_ = nullptr;
+    mutable std::mutex mutex_;
+};
+
+template<typename... Args>
+void LogError(std::string_view format, Args&&... args) {
+    kodi::Log(ADDON_LOG_ERROR, kodi::tools::StringUtils::Format(format, std::forward<Args>(args)...));
 }
-#endif /* __globals_hpp__ */
+
+template<typename... Args>
+void LogInfo(std::string_view format, Args&&... args) {
+    kodi::Log(ADDON_LOG_INFO, kodi::tools::StringUtils::Format(format, std::forward<Args>(args)...));
+}
+
+template<typename... Args>
+void LogNotice(std::string_view format, Args&&... args) {
+    kodi::Log(ADDON_LOG_WARNING, kodi::tools::StringUtils::Format(format, std::forward<Args>(args)...));
+}
+
+template<typename... Args>
+void LogDebug(std::string_view format, Args&&... args) {
+    kodi::Log(ADDON_LOG_DEBUG, kodi::tools::StringUtils::Format(format, std::forward<Args>(args)...));
+}
+
+std::unique_ptr<kodi::vfs::CFile> OpenFile(std::string_view path, unsigned int flags = 0);
+
+} // namespace Globals
+
+#endif // GLOBALS_HPP
